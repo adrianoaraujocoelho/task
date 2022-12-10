43301af4d9f94921aa16b84921306d7126c5ce88
adriano.dev <adriano.coelho@dataside.com.br>
2022-12-09 22:14:38 -0300

    tela-cadastro

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Feed from "./pages/feed";
+import Sign from "./pages/sign";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/feed" element={<Feed />} />
+          <Route path="/Sign" element={<Sign />} />
         </Routes>
       </Router>
     </>
